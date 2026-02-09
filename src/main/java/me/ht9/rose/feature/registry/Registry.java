f3870ebe9dc1644d48739763b9030d7910b2afc3
Intiha <gund4422@gmail.com>
2026-02-09 17:03:57 +0600

    Update Registry.java

--- src/main/java/me/ht9/rose/feature/registry/Registry.java
+++ src/main/java/me/ht9/rose/feature/registry/Registry.java
@@ -14,7 +14,7 @@ import me.ht9.rose.feature.module.modules.client.irc.IRC;
 import me.ht9.rose.feature.module.modules.client.mainmenu.MainMenu;
 import me.ht9.rose.feature.module.modules.client.togglemsg.ToggleMsg;
 import me.ht9.rose.feature.module.modules.combat.aura.Aura;
-import me.ht9.rose.feature.module.modules.dupes.ChestDupe
+import me.ht9.rose.feature.module.modules.dupes.ChestDupe;
 import me.ht9.rose.feature.module.modules.exploit.boatfly.BoatFly;
 import me.ht9.rose.feature.module.modules.exploit.fastportal.FastPortal;
 import me.ht9.rose.feature.module.modules.exploit.infdurability.InfDurability;
